cf5b7e55d726c22d98a38b0695d6c46844a8c188
Alexsander Akers <a2@pandamonia.us>
2011-09-04 14:06:05 -0400

    Updated license

--- A2BackgroundTableView.m
+++ A2BackgroundTableView.m
@@ -1,9 +1,53 @@
 //
-//  A2BackgroundTableView.m
-//
-//  Created by Alexsander Akers on 8/18/11.
-//  Copyright 2011 Pandamonia LLC. All rights reserved.
-//
+//  File: A2BackgroundTableView.h
+//  Abstract: A UITableView subclass that allows a scrolling and tiled background image
+//      to be displayed behind grouped cells.
+//  
+//  Disclaimer: IMPORTANT: This Pandamonia software is supplied to you by
+//  Alexsander Akers on behalf of and by Pandamonia LLC ("Pandamonia") in
+//  consideration of your agreement to the following terms, and your use,
+//  installation, modification or redistribution of this Pandamonia software
+//  constitutes acceptance of these terms. If you do not agree with these terms,
+//  please do not use, install, modify or redistribute this Pandamonia software.
+//  
+//      In consideration of your agreement to abide by the following terms, and
+//  subject to these terms, Pandamonia grants you a personal, non-exclusive
+//  license, under Pandamonia's copyrights and intellectual property rights in
+//  this original Pandamonia software (the "Pandamonia Software"), to use,
+//  reproduce, modify and redistribute a part of or the entirety of the
+//  Pandamonia Software, with or without modifications, in source and/or binary
+//  forms, or by any electronic or mechanical means, including information and
+//  retrieval systems (collectively, "to Redistribute"); provided that if you
+//  Redistribute the Pandamonia Software, you must retain this notice and the
+//  following text and disclaimers in all such redistributions of the Pandamonia
+//  Software, including the documentation and/or other materials provided with
+//  the distribution.
+//  
+//      Neither the name, trademarks, service marks or logos of Pandamonia may
+//  be used to endorse or promote products derived from the Pandamonia Software
+//  without specific prior written permission from Pandamonia. Except as
+//  expressly stated in this notice, no other rights or licenses, express or
+//  implied, are granted by Pandamonia herein, including but not limited to any
+//  patent rights that may be infringed by your derivative works or by other
+//  works in which the Pandamonia Software may be incorporated.
+//  
+//      The Pandamonia Software is provided by Pandamonia on an "AS IS" basis.
+//  PANDAMONIA MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT
+//  LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND
+//  FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE PANDAMONIA SOFTWARE OR ITS
+//  USE AND OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+//  
+//      IN NO EVENT SHALL PANDAMONIA BE LIABLE FOR ANY SPECIAL, INDIRECT,
+//  INCIDENTAL OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+//  OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+//  MODIFICATION AND/OR DISTRIBUTION OF THE PANDAMONIA SOFTWARE, HOWEVER CAUSED
+//  AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT
+//  LIABILITY OR OTHERWISE, EVEN IF PANDAMONIA HAS BEEN ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//  
+//  Copyright (c) 2011 Pandamonia. All rights reserved.
+//  
 
 #import "A2BackgroundTableView.h"
 
